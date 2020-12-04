f7f200d0f8af2d1dc3b245263d443a6c0b08179c
sgilgen <gilgen@puzzle.ch>
2020-12-04 12:52:26 +0100

    Continue day3.2

--- 2020/Day3.2/day3.rb
+++ 2020/Day3.2/day3.rb
@@ -1,10 +1,18 @@
 file = File.open("input.txt")
 lines = file.read.split("\n")
-x = 3
+x = 7
 trees = 0
 i = 0
+for lines in lines.length do
+    
+end
 lines.each do |line|
     trees += 1 if line[i].eql?('#')
     i = (i + x) >= line.length - 1 ? (i + x) - line.length : i + x
 end
 pp trees
+
+# 3:1 294
+# 1:1 75
+# 5:1 79
+# 7:1 85
