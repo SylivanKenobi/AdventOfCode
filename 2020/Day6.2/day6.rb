bbbeb60c1ac71a3f20de2457d68df298da90ba21
Sylvain <sypsilon@hotmail.com>
2020-12-22 22:20:51 +0100

    Make day 6.2 nicer

--- 2020/Day6.2/day6.rb
+++ 2020/Day6.2/day6.rb
@@ -2,20 +2,12 @@ file = File.open('input.txt')
 input_lines = file.read.split("\n\n")
 counter = 0
 input_lines.each do |input_line|
-    split_line = input_line.split.map { |l| l.split('') }
     map = {}
-    split_line[0].each { |l|  map.merge!("#{l}" => 0) }
-    split_line.each do |variable|
-        variable.each do |letter|
-            if map.key?(letter)
-                map[letter] += 1
-            end
-        end
+    block = input_line.split.map { |l| l.split('') }
+    block[0].each { |l|  map.merge!("#{l}" => 1) }
+    block[1, block.length - 1].each do |line|
+        line.each { |l| map[l] += 1 if map.key?(l) }
     end
-    map.each do |k, v|
-        if v == split_line.length
-            counter += 1
-        end
-    end
+    map.each { |k, v| counter += 1 if v == block.length }
 end
 pp counter
