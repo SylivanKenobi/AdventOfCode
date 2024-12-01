2fdc7747587e20f87535fc40b44f51eb26799ddb
SylivanKenobi <sypsilon@hotmail.com>
2024-12-01 10:22:24 +0100

    solve day 1

--- 2024/day1/part1.go
+++ 2024/day1/part1.go
@@ -3,21 +3,57 @@ package day1_part1
 import (
 	"adventofcode/utils"
 	"fmt"
-	"regexp"
+	"math"
+	"sort"
 	"strconv"
+	"strings"
 )
 
-// Executes the puzzle for all days
 func Part1(file string) int {
-	lines := utils.ReadFileByLine("1_1.txt")
-	calibration := 0
+	lines := utils.ReadFileByLine(file)
+	var leftList []int
+	var rightList []int
 	for _, line := range lines {
-		rgx := regexp.MustCompile(`\d`)
-		numbers := rgx.FindAllString(line, -1)
-		coordinate, _ := strconv.Atoi(numbers[0] + numbers[len(numbers)-1])
-		calibration += coordinate
-
+		sides := strings.Split(line, "   ")
+		left, _ := strconv.Atoi(sides[0])
+		leftList = append(leftList, left)
+		right, _ := strconv.Atoi(sides[1])
+		rightList = append(rightList, right)
 	}
-	fmt.Println(calibration)
-	return calibration
+	sort.Sort(sort.IntSlice(rightList))
+	sort.Sort(sort.IntSlice(leftList))
+
+	totalDist := 0
+	for i, _ := range leftList {
+		dist := leftList[i] - rightList[i]
+		totalDist = int(math.Abs(float64(dist))) + totalDist
+	}
+	fmt.Println(totalDist)
+	return totalDist
+}
+
+func Part2(file string) int {
+	lines := utils.ReadFileByLine(file)
+	var leftList []int
+	var rightList []int
+	for _, line := range lines {
+		sides := strings.Split(line, "   ")
+		left, _ := strconv.Atoi(sides[0])
+		leftList = append(leftList, left)
+		right, _ := strconv.Atoi(sides[1])
+		rightList = append(rightList, right)
+	}
+
+	similarity := 0
+	for _, left := range leftList {
+		matches := 0
+		for _, right := range rightList {
+			if left == right {
+				matches += 1
+			}
+		}
+		similarity += (left * matches)
+	}
+	fmt.Println(similarity)
+	return similarity
 }
--- 2024/main.go
+++ 2024/main.go
@@ -4,6 +4,7 @@ import ( // "context"
 	// "dagger/advent-of-code/internal/dagger"
 	// "dagger/advent-of-code/internal/dagger"
 	day1_part1 "adventofcode/day1"
+	"fmt"
 	// "strconv"
 	// "strings"
 )
@@ -12,5 +13,8 @@ type AdventOfCode struct{}
 
 // Executes the puzzle for all days
 func main() {
-	day1_part1.Part1("1.txt")
+	fmt.Println("day 1 part 1")
+	day1_part1.Part1("1_1.txt")
+	fmt.Println("day 1 part 2")
+	day1_part1.Part2("1_1.txt")
 }
